fe3c7bc900ee79f0ca7a01e4d916c43e3b6df497
soumyamehar29 <soumyamehar154@gmail.com>
2026-04-20 23:50:56 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,6 @@
+//The following is a simple program that displays the message “Hello, World!” on the screen.
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
