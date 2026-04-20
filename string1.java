6ed193c42b8fc55385858edd75be19ef004da836
soumyamehar29 <soumyamehar154@gmail.com>
2026-04-20 23:55:36 +0530

    Update string1.java

--- string1.java
+++ string1.java
@@ -2,8 +2,8 @@ public class String_Example1 {
 	public static void main(String[] args) {
 		String str = "Nagpur";
 		System.out.println(str);
-		String fname = "Kanak ";
-		String lname = "Tembhurne";
+		String fname = "Soumya";
+		String lname = "Mehar";
 		System.out.println(fname+lname);
 		System.out.println("Length of the string str is "+str.length());}
 }
