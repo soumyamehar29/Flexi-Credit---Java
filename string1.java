5e6b5d474ce821a62e18ad3f322d60e756417f1e
soumyamehar29 <soumyamehar154@gmail.com>
2026-04-20 23:55:00 +0530

    Create string1.java

---
+++ string1.java
@@ -0,0 +1,9 @@
+public class String_Example1 {
+	public static void main(String[] args) {
+		String str = "Nagpur";
+		System.out.println(str);
+		String fname = "Kanak ";
+		String lname = "Tembhurne";
+		System.out.println(fname+lname);
+		System.out.println("Length of the string str is "+str.length());}
+}
