3f3d87403eabab4a99d68391755e5cc6c47bad96
soumyamehar29 <soumyamehar154@gmail.com>
2026-04-20 23:56:12 +0530

    Create string2.java

---
+++ string2.java
@@ -0,0 +1,12 @@
+public class String_Example1 {
+	public static void main(String[] args) {
+		String str = "Nagpur";
+		System.out.println(str);
+		int len = str.length();
+		System.out.println("Length of the string str is "+len);
+		String upper_str = str.toUpperCase();
+		System.out.println("Upper Case String is : "+upper_str);
+		String lower_str = str.toLowerCase();
+		System.out.println("Lower Case String is : "+lower_str);
+	}
+}
